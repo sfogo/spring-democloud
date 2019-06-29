1fdff0ae696dd168a144c95423dcce7f85a93745
Olivier Gorostis <ogorostis@stubhub.com>
2019-06-28 17:51:59 -0700

    Changed dependency

--- generate-traffic.py
+++ generate-traffic.py
@@ -1,36 +1,38 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python2.7
 
 import sys
 import random
 import time
-import math
-import http.client
+import httplib
 
 minDelay = 0.010
 m1ItemsPath = "/gateway/m1/items"
 m2ItemsPath = "/gateway/m2/items"
 
+
 # =====================
 # Generate Traffic
 # =====================
-def feed(count,gateway):
-    for e in range(0,count):
-        getResource(gateway, m1ItemsPath, "m1-" + str(e))
+def feed(count, gateway):
+    for e in range(0, count):
+        get_resource(gateway, m1ItemsPath, "m1-" + str(e))
         time.sleep(minDelay + (0.001 * random.randint(0,100)))
-        getResource(gateway, m2ItemsPath, "m2-" + str(e))
+        get_resource(gateway, m2ItemsPath, "m2-" + str(e))
         time.sleep(minDelay + (0.001 * random.randint(0,100)))
-        if (e % 7 == 0):
-            getResource(gateway, m1ItemsPath, "x%20y")
-            getResource(gateway, m2ItemsPath, "z%20t")
+        if e % 7 == 0:
+            get_resource(gateway, m1ItemsPath, "x%20y")
+            get_resource(gateway, m2ItemsPath, "z%20t")
+
 
 # =====================
 # Get Resource
 # =====================
-def getResource(host,path,resource):
-    c = http.client.HTTPConnection(host)
-    c.request("GET",path + "/" + resource)
-    response = c.getresponse();
+def get_resource(host, path, resource):
+    c = httplib.HTTPConnection(host)
+    c.request("GET", path + "/" + resource)
+    response = c.getresponse()
     print(response.read())
+
 
 # =====================
 # Main
@@ -39,12 +41,12 @@ for i in range(0, len(sys.argv)):
     print ('Arg#', i, sys.argv[i])
 
 argc = len(sys.argv)
-if (argc >= 2):
+if argc >= 2:
     random.seed()
     gateway = "localhost:8099"
-    if (argc >= 3 and "-docker"==sys.argv[2]):
+    if argc >= 3 and "-docker"==sys.argv[2]:
         gateway = "localhost"
-    feed(int(sys.argv[1]),gateway)
+    feed(int(sys.argv[1]), gateway)
 else:
     print ('Syntax error. Number of events is required.')
     print (sys.argv[0], '<Count> [-docker]')
